1d69925803b59ceae14a2c8d59b82f68a0a7c609
Ahmed Nasser Zaki <anm1611999@gmail.com>
2022-03-26 19:33:21 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIB4hHVRELGyQmeG_irCkHvL3W-ZWewFU",
-  authDomain: "shopping-app-dev-dc0df.firebaseapp.com",
-  projectId: "shopping-app-dev-dc0df",
-  storageBucket: "shopping-app-dev-dc0df.appspot.com",
-  messagingSenderId: "51928212965",
-  appId: "1:51928212965:web:50aa0962dbd7a6158c3b0c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
